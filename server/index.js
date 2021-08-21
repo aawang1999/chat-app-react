b18d99a98791c45919a68a96cb7c737a93e6d282
Tristan Wang <aawang99@gmail.com>
2021-08-20 22:13:45 -0700

    feat: join functionality

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+io.on('connection', (socket) => {
+  console.log('We have a new connection.')
+  socket.on('disconnect', () => {
+    console.log('User has disconnected.')
+  })
+})
+
 app.use(router)
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}.`))
