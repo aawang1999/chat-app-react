2514afefb0544484347dbe179df9af81f64cfe68
Tristan Wang <aawang99@gmail.com>
2021-08-22 22:50:17 -0700

    feat: dynamic info bar

---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import closeIcon from '../../icons/closeIcon.png'
+import onlineIcon from '../../icons/onlineIcon.png'
+import './InfoBar.css'
+
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img src={onlineIcon} alt="Online Image" className="onlineIcon" />
+      <h3>{room}</h3>
+    </div>
+    <div className="rightInnerContainer">
+      <a href="/"><img src={closeIcon} alt="Close Image" /></a>
+    </div>
+  </div>
+)
+
+export default InfoBar
